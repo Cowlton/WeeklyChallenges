d6d7b2b42291223546cf6b9c889ef6bba1523141
Colton Briggs <wolflink013@gmail.com>
2025-08-28 16:47:05 -0700

    worked on challange set 4

--- ChallengesWithTestsMark8.Tests/obj/Debug/net9.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
+++ ChallengesWithTestsMark8.Tests/obj/Debug/net9.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb8712712d2f13a1512234ab1388fff947cfdf66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39013631af85db2cfc89ffa5e0e84f4859962d76")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +9,84 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+           
+            var result = 0;
+            
+            foreach (var n in numbers)
+            {
+                if (n % 2 == 0)
+                {
+                    result += n;
+                }
+                else
+                {
+                    result -= n;
+                }
+            };
+            return result;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var strings = new List<string>(){str1, str2, str3, str4};
+            var shortest = strings.OrderBy(s => s.Length).First();
+            return shortest.Length;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var list = new List<int>() { number1, number2, number3, number4 };
+            return list.OrderBy(s => s).First();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1 ? true : false;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            if (input == null){return false;}
+            return Regex.IsMatch(input, @"^-?\d+(\.\d+)?$");
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            if (objs == null || objs.Length == 0){return true;}
+            int nullCount = objs.Count(obj => obj == null);
+            return nullCount > objs.Length / 2;
+            
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null){return 0;}
+            var evens = new List<int>();
+            foreach (var n in numbers)
+            {
+                if (n % 2 == 0)
+                {
+                    evens.Add(n);
+                }
+            }
+            if (evens.Count == 0){return 0;}
+            return evens.Average();
+
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
